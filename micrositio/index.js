47a1732c0a590d343f34e74579b16b2fc4b90164
Fabio Giacometti <fabio.giacometti@ross.com.ar>
2020-04-14 13:56:28 -0300

    pretty ready

--- micrositio/index.js
+++ micrositio/index.js
@@ -29,11 +29,11 @@ function scrollFunction() {
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.transform = "translateX(250px)";
+    // document.getElementById("main").style.marginLeft = "250px";
   }
   
   /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.transform = "translateX(0px)";
+    // document.getElementById("main").style.marginLeft = "0";
   }
